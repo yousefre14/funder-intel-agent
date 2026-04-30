1113adcab05515e0ecb2cf688c3547ba6c766f40
Yousef Reda <yousef.jo.reda14@gmail.com>
2026-04-30 16:22:21 +0300

    Change initial_sidebar_state to 'auto'

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.set_page_config(
     page_title="Funder Intelligence Agent",
     page_icon="🔍",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="auto",
 )
 
 
