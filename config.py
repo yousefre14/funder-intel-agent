d588c75e3360eda5f4f79501a01e9c67a8f73cfd
yousefre14 <yousef.jo.reda14@gmail.com>
2026-05-04 15:46:41 +0300

    readded groq

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ load_dotenv()
 
 #API Keys
 GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
-#GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 TAVILY_API_KEY=os.getenv("TAVILY_API_KEY")
 
 GEMINI_Model= "gemini-2.0-flash"
-#GROQ_MODEL = "llama-3.3-70b-versatile" 
+GROQ_MODEL = "llama-3.3-70b-versatile" 
 MAX_TOKENS = 4096
 
 
 ## cost tracking in case we use paid APIs in the future, for now we just want to track usage to stay within free tiers
 
-TOTAL_REQUESTS = {"gemini": 0, "tavily": 0}
+TOTAL_REQUESTS = {"gemini": 0, "groq": 0, "tavily": 0}
 
 def track_usage(provider):
     """Track API usage so we stay within free tiers"""
